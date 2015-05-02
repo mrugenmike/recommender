ea69254343384a0dfbaa989b870b2e6b7e7a1ff9
Mrugen Deshmukh <mrugenmike@gmail.com>
2015-05-02 06:27:14 -0700

    modified the script to read from the people collections

--- scripts/generateCareerPath.js
+++ scripts/generateCareerPath.js
@@ -1,5 +1,13 @@
 
-var distinctCompanies = db.getSiblingDB("pronet").getCollection("careerPath").distinct("career.company");
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
+
+var distinctCompanies = db.getSiblingDB("pronet").getCollection("people").distinct("relationships.firm.name");
 var levels = {"1":["software engineer","software developer","programmer analyst","application developer","Systems analyst"],
 			  "2":["senior software engineer","staff software engineer","principal software engineer","member of technical staff","Senior Systems analyst"],
 			  "3":["Technical Lead","Architect","solutions architect","senior architecture","systems architect"],
@@ -14,18 +22,19 @@ function generateCareerPaths(numberOfRecords){
 		var pronet = db.getSiblingDB("pronet");
 		var careerPaths = db.getCollection("careerPath");
 		var paths = [];
-		var level = Math.ceil(Math.random()*levels.length);
-	for(var index = 0; index < level; index++){
-		var levelElment = Math.ceil(Math.random()*levels[index].length);
+		var level = Math.ceil(Math.random()*Object.size(levels));
+	for(index = 1; index <= level; index++){
+		print("levels[index]:"+levels[index]+" index:"+index);
+		var levelElment = Math.ceil(Math.random()*(levels[index].length));
+		print("\n pushing now");
 		paths.push({
-			"title":level[levelElment]||level[levelElment-1],
+			"title":levels[index][levelElment]||levels[index][levelElment-1],
 			"company":distinctCompanies[Math.ceil(Math.random()*distinctCompanies.length)]		
 		});
 	}
 	var careerPathForOneIndividual = {
 	"fname" : "userFname-"+new Date().getTime().toString().substr(6,12),
 	"lname" : "userLname-"+new Date().getTime().toString().substr(6,12),
-	"permalink" : this.fname+"-"+this.lname,
 	"career" :paths
     }
     careerPaths.insert(careerPathForOneIndividual);
